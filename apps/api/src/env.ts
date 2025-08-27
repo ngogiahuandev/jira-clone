752afa0085124d4e20dfba512231f539fcf28604
Ngo Gia Huan <ngogiahuan23122003@gmail.com>
2025-08-27 23:42:00 +0700

    add shadcn for web and env for hono

---
+++ apps/api/src/env.ts
@@ -0,0 +1,16 @@
+import "dotenv/config";
+import z from "zod";
+
+const envSchema = z.object({
+  DATABASE_URL: z.string(),
+  REDIS_URL: z.string(),
+  JWT_SECRET: z.string(),
+  ACCESS_TOKEN_TTL: z.string(),
+  REFRESH_TOKEN_TTL_SECONDS: z.coerce.number(),
+  REFRESH_COOKIE_NAME: z.string(),
+  PORT: z.coerce.number(),
+  NODE_ENV: z.enum(["development", "production"]),
+});
+
+export type Env = z.infer<typeof envSchema>;
+export const env = envSchema.parse(process.env);
--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,7 +1,10 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
+import { logger } from "hono/logger";
+import { env } from "./env";
 
 const app = new Hono();
+app.use(logger());
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
@@ -10,9 +13,9 @@ app.get("/", (c) => {
 serve(
   {
     fetch: app.fetch,
-    port: 3000,
+    port: env.PORT,
   },
   (info) => {
     console.log(`Server is running on http://localhost:${info.port}`);
-  },
+  }
 );
